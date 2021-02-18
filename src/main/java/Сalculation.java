c7a66b17bfd89577332b8058e73acbec6f7de0db
Kopper <zalans.serg@gmail.com>
2021-02-18 20:38:26 +0300

    HWles1

---
+++ src/main/java/Сalculation.java
@@ -0,0 +1,17 @@
+public class Сalculation {
+    //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+    //где a, b, c, d – аргументы этого метода, имеющие тип float.
+
+    public static void main(String[] args) {
+
+    }
+
+    public static float calculation() {
+        float a = 2.33f;
+        float b = 4.21f;
+        float c = 8.2354f;
+        float d = 9.523f;
+
+        return a * (b + (c / d));
+    }
+}
