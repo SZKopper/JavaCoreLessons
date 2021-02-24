09eac71455ddea36d13582fbb2f091e63305be4e
Sergei Zalans <Zalans.serg@gmail.com>
2021-02-24 16:13:44 +0300

    Add ex6

--- src/main/java/ru/gb/HW/Less2/ArmsEquality.java
+++ src/main/java/ru/gb/HW/Less2/ArmsEquality.java
@@ -1,10 +1,35 @@
 package ru.gb.HW.Less2;
 
 public class ArmsEquality {
-    // Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
-    // если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
-    // checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят.
+    // Написать метод, в который передается не пустой одномерный целочисленный массив,
+    // метод должен вернуть true, если в массиве есть место,
+    // в котором сумма левой и правой части массива равны.
+    // Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+    // checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||,
+    // эти символы в массив не входят.
     public static void main(String[] args) {
+        int[] arr = {2, 2, 2, 1, 2, 2, 10, 1};
+        arrEmpty(arr);
 
+    }
+
+    public static boolean arrEmpty(int[] arr){
+        int lSum = arr[0];
+        int rSum = arr[arr.length - 1];
+            for (int i = 1; i < arr.length; i++) {
+                lSum = lSum + arr[i];
+                for (int j = arr.length - 1; j >= 0; j--) {
+                    if (rSum < lSum) {
+                        rSum = rSum + arr[j- 1];
+                    } else {
+                        break;
+                    }
+                }
+                if (lSum == rSum){
+                    break;
+                }
+            }
+        System.out.println(lSum + " " + rSum);
+        return lSum == rSum;
     }
 }
