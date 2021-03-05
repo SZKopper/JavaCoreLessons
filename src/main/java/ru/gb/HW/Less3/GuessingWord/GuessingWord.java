edd410caaffe5e979ae251b34cb0da377d656b7e
Kopper <zalans.serg@gmail.com>
2021-03-05 17:25:35 +0300

    Add HW less3 task1

---
+++ src/main/java/ru/gb/HW/Less3/GuessingWord/GuessingWord.java
@@ -0,0 +1,9 @@
+package ru.gb.HW.Less3.GuessingWord;
+
+public class GuessingWord {
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        String guessWord
+    }
+}
