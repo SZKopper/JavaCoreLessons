d74e2e73d7cbe27b55f0005bcb10b2a1cdc25f0a
Sergei Zalans <Zalans.serg@gmail.com>
2021-03-05 18:22:50 +0300

    Add change at ex1

--- src/main/java/ru/gb/HW/Less3/GuessingGame/Guessing.java
+++ src/main/java/ru/gb/HW/Less3/GuessingGame/Guessing.java
@@ -1,4 +1,4 @@
-package ru.gb.HW.Less3.GuessingGame;
+package ru.gb.HW.Less3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,57 +9,43 @@ public class Guessing {
     //При каждой попытке компьютер должен сообщить, больше ли указанное пользователем число, чем загаданное, или меньше.
     //После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static int counter = 1;
-
-
+    private static int enter;
     public static void main(String[] args) throws IOException {
-        System.out.println("Введите число от 0 до 9");
-        int enter = Integer.parseInt(reader.readLine()); // Переменная хранящая пользовательский ввод
-        comparison(enter);// Метод принимающий пользовательский ввод
-        if (counter == 3){
-            System.out.println("Вы проиграли.\nПовторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет");
-            enter = Integer.parseInt(reader.readLine());
-            if (enter > 1 || enter < 0){
-                System.out.println("Введите 1 – если хотите повторить / 0 – если хотите закончить игру");
-            } else if (enter == 1) {
-                System.out.println("Введите число от 0 до 9");
-                enter = Integer.parseInt(reader.readLine());
-                comparison(enter);
-            } else if (enter == 0){
-                System.out.println("Игра окончена!");
-            }
+        int randomNum =  (int) (Math.random() * 9);
+        int count = 3;
+        comparison("Введите число от 0 до 9", count, randomNum);
+        if (enter == randomNum){
+            System.out.println("Вы выиграли");
+        } else {
+            System.out.println("Вы проиграли");
         }
-
-    }
-
-    private static void comparison(int enter) throws IOException {
-        int randomNum =  (int) (Math.random() * 9); // Переменна хранит рандомное число от 0 до 9
-
-        // Цикл выполняется пока counter < 3
-        do {
-            counter++;
-            if (enter > randomNum){ // Проверяем больше ли введенное число чем загаданное
-                System.out.println("Введенное число больше загаданного!\nПовторите попытку"); // Если больше выводим текст вывдим этот текст и выходим из цикла
-            } else if (enter < randomNum){ // Проверяем меньще ли введенное число чем загаданное
-                System.out.println("Введенное число меньше загаданного!\nПовторите попытку"); // Если меньше выводим текст вывдим этот текст и выходим из цикла
-            } else {// Проверяем равно ли введенное число загаданному
-                System.out.println("Вы угадали!\nПовторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)"); // Если загаданное и введенное равны вывдим этот текст
-                enter = Integer.parseInt(reader.readLine());
-                if (enter > 1 || enter < 0){
-                    System.out.println("Введите 1 – если хотите повторить / 0 – если хотите закончить игру");
-                } else if (enter == 1) {
-                    System.out.println("Введите число от 0 до 9");
-                    enter = Integer.parseInt(reader.readLine());
-                    comparison(enter);
-                } else if (enter == 0){
-                    System.out.println("Игра окончена!");
-                }
-            }
-
+        System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
+        enter = Integer.parseInt(reader.readLine());
+        if (enter > 1 || enter < 0){
+            System.out.println("Введите 1 – если хотите повторить / 0 – если хотите закончить игру");
             enter = Integer.parseInt(reader.readLine());
-        } while ( counter < 3);
+        } else if (enter == 1) {
+            comparison("Введите число от 0 до 9", count, randomNum);
+        } else if (enter == 0){
+            System.out.println("Игра окончена!");
+        }
+    }
+
+    private static void comparison(String start, int count, int randomNum) throws IOException {
+        System.out.println(start);
+        do {
+            enter = Integer.parseInt(reader.readLine());
+            count--;
+            if (enter == randomNum){
+                System.out.println("Вы угадали!");
+                break;
+            } else if (enter > randomNum) {
+                System.out.println("Введенное число больше загаданного!");
+            } else if (enter < randomNum) { // Проверяем меньще ли введенное число чем загаданное
+                System.out.println("Введенное число меньше загаданного!");
+            }
+        }while (count > 0) ;
 
 
     }
-
 }
