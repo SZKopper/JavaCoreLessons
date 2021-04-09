bdee2fbde5d9c301c6a1f5c9a8dfe6f916587ace
Sergei Zalans <Zalans.serg@gmail.com>
2021-04-09 11:11:01 +0300

    Add HW12

--- src/main/java/ru/gb/HW/Less12/App.java
+++ src/main/java/ru/gb/HW/Less12/App.java
@@ -1,12 +1,17 @@
 package ru.gb.HW.Less12;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        //Создаем объект класса HW
         HW hw = new HW();
+        //Вызываем первый метод (без потоков)
         hw.method1();
-        //Thread t1 = new Thread(new HW.HWRunnableClass("Название потока"));
-
+        //Вызываем второй метод (с потоками)
         hw.method2();
-
     }
 }
--- src/main/java/ru/gb/HW/Less12/HW.java
+++ src/main/java/ru/gb/HW/Less12/HW.java
@@ -6,58 +6,68 @@ public class HW {
     static final int SIZE = 10_000_000;
     static final int HALF = SIZE/2;
 
+    //Метод для пересчета элементов в массиве
     public void arrCalculate(float[] arr){
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
 
+    //Первый метод выполняющий расчет без потоков
     public void method1(){
-        float[] arr1 = new float[SIZE];
-        Arrays.fill(arr1, 1);
-        long a = System.currentTimeMillis();
-        //System.out.println(a/ 1000);
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        //System.out.println(System.currentTimeMillis()/ 1000);
-        double b = ((double) System.currentTimeMillis() - a) / 1000;
-        System.out.println("Method1 work time: " + b + " sec");
+        float[] arr1 = new float[SIZE]; //Создали массив размером SIZE
+        Arrays.fill(arr1, 1); //Заполнили его единицами
+        long a = System.currentTimeMillis(); //Засекли время до пересчета массива
+        arrCalculate(arr1); //Выполняем пересчет
+        double b = ((double) System.currentTimeMillis() - a) / 1000; // Получаем время работы расчета
+        System.out.println("Method1 work time: " + b + " sec"); //Выводи время работы первого метода
     }
 
-
+    //Второй метод выполняющий расчет в потоках
     public void method2(){
-        float[] arr2 = new float[SIZE];
-        Arrays.fill(arr2, 1);
+        float[] arr2 = new float[SIZE]; //Создали массив размером SIZE
+
+        Arrays.fill(arr2, 1); //Заполнили его единицами
+
+        //Создаем два массива размером HALF
         float[] arr3 = new float[HALF];
         float[] arr4 = new float[HALF];
-        long a = System.currentTimeMillis();
+
+        long a = System.currentTimeMillis(); //Засекли время до деления массива и пересчета
+
+        //Делим массив arr1 на два arr3 и arr4
         System.arraycopy(arr2,0,arr3,0,HALF);
         System.arraycopy(arr2,5,arr4,0,HALF);
 
-        double finalA = (double) a / 1000;
+        double finalA = (double) a / 1000; //Засекаем время до начала работы первого потока
+
+        //Создаем первый поток
         Thread t1 = new Thread(() -> {
             System.out.println("First thread time before start: " + finalA + " sec");
-            for (int i = 0; i < arr3.length; i++) {
-                arr3[i] = (float)(arr3[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-            double b = (double) System.currentTimeMillis() / 1000;
+
+            arrCalculate(arr3); //Выполняем пересчет для первой половины
+
+            double b = (double) System.currentTimeMillis() / 1000; //Засекаем время кончания работы первого потока
             System.out.println("First thread end time: " + b + " sec");
         });
 
-        double finalA1 = (double) a / 1000;
+        double finalA1 = (double) a / 1000;//Засекаем время до начала работы второго потока
+
+        //Создаем второй поток
         Thread t2 = new Thread(() -> {
             System.out.println("Second thread time before start: " + finalA1 + " sec");
-            for (int i = 0; i < arr4.length; i++) {
-                arr4[i] = (float)(arr4[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-            double b = (double) System.currentTimeMillis() / 1000;
+
+            arrCalculate(arr4);//Выполняем пересчет для второй половины
+
+            double b = (double) System.currentTimeMillis() / 1000;//Засекаем время кончания работы второго потока
             System.out.println("Second thread end time: " + b + " sec");
         });
 
+        //Запускаем первый и второй потоки
         t1.start();
         t2.start();
 
+        //Приостанавливаем время работы потока main пока потоки t1 и t2 не закончат работу
         try {
             t2.join();
             t1.join();
@@ -66,10 +76,15 @@ public class HW {
         }
 
         a = System.currentTimeMillis();
+
+        //Собираем половинки массива в один
         System.arraycopy(arr3,0,arr2,0,HALF);
         System.arraycopy(arr4,0,arr2,HALF,HALF);
+
+        //Расчитываем и выводим на экран время сборки в 1 массив
         System.out.println("Collection time in one array: " + (((double) System.currentTimeMillis() - a) / 1000) + " sec");
 
+        //РАсчитываем и выводим время работы второго метода
         double b = ((double) System.currentTimeMillis() - a) / 1000;
         System.out.println("Method2 work time: " + b + " sec");
     }
